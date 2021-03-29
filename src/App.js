1bfb88c5fdc3d13fbc413c308ae8cde3dae25f3a
Mina Nagy <minanagy1993@gmail.com>
2021-03-29 19:45:45 +0200

    Enable Routing Pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { React } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import{BrowserRouter,Switch,Route} from 'react-router-dom'
 import Navbar from './Component/Navbar.js'
 import AddTask from './Component/AddTask';
 import TaskList from './Component/TaskList.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div>
-      {/* <h1 className="text-center display-5">Hello Mina</h1> */}
-      <Navbar />
-      <div className="container">
-        <div className="row">  
-            <div className="col-10 mx-auto">
-                <AddTask />
-                <TaskList />
-            </div>
-        </div> 
-      </div>
+      <BrowserRouter>
+              <Navbar />
+              <div className="container">
+                <div className="row">  
+                    <div className="col-10 mx-auto">
+                      <Switch>
+                          <Route exact path="/" component={TaskList} />
+                          <Route exact path="/add-task" component={AddTask} />
+                      </Switch>
+                    </div>
+                </div> 
+              </div>
+      </BrowserRouter>
     </div>
   );
 }

79ba5611703966ab2d52f0cb1b2b8c3069c7e62b
Mina Nagy <minanagy1993@gmail.com>
2021-03-29 19:24:03 +0200

    Add Navbar Html Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { React } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Component/Navbar.js'
+import AddTask from './Component/AddTask';
 function App() {
   return (
     <div>
-      <h1>Hello Mina</h1>
-
+      {/* <h1 className="text-center display-5">Hello Mina</h1> */}
+      <Navbar />
+    
     </div>
   );
 }

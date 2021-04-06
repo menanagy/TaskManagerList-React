5874087ed3a58a5420653fa135aa74ab8c910a50
Mina Nagy <minanagy1993@gmail.com>
2021-04-06 21:16:35 +0200

    Finishing Local Storage

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,36 +1,54 @@
-import React,{useState,createContext}from 'react';
+import React,{useState,createContext,useEffect}from 'react';
 export const TaskContext=createContext();
-const initialState=[
+/*const initialState=[
     {title:"First Task",id:1},
     {title:"Second Task",id:2},
     {title:"Third Task",id:3}
-]
+]*/ //Replacing By Using Local Storage
 export const TaskContextProvider =(props)=>{
-    const[tasks,setTasks]=useState(initialState)
+    // const[tasks,setTasks]=useState([]);
+
+    const[tasks,setTasks]=useState(JSON.parse(localStorage.getItem('tasks_list'))||[]);
     const[itemEdit,setItemEdit]=useState(null)
     // const[tasks,setTasks]=useState({title:"First Task",id:1},{title:"Second Task",id:2},{title:"Third Task",id:3})
+    
+    useEffect(() => {
+        localStorage.setItem('tasks_list', JSON.stringify(tasks))
+     }, [tasks])
+     
+    
+    //When Press Delet Button
     const removeItem=(id)=>{
         console.log(id);
         const newTask=tasks.filter((item)=>item.id !== id);
         console.log(newTask);
         setTasks(newTask);
     }
+
+    //Add Task
+
     const addTask=(task)=>{
        console.log(task);
         setTasks([...tasks,task])//tasks=tasks+task
     }
+    
+    //Get Element when press Edit Button 
     const findItem=(id)=>{
         console.log(`Edit : ${id}`);
         const task=tasks.find((item)=>item.id===id);
         console.log(task);
         setItemEdit(task);
     }
+    
+    
     const editTask=(task)=>{
         console.log(task);
         const tasksEdited=tasks.map((item)=> item.id === task.id ? task : item)
         setTasks(tasksEdited);
         setItemEdit(null);
     }
+    
+
     return(
         <TaskContext.Provider value={{tasks,removeItem,addTask,findItem,itemEdit,editTask}}>
             {props.children}

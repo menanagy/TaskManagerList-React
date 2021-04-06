966f0c0a9c3160ce3605c47987455fbbef749352
Mina Nagy <minanagy1993@gmail.com>
2021-04-06 23:37:40 +0200

    Add Style Css

--- src/App.js
+++ src/App.js
@@ -7,26 +7,30 @@ import EditTask from './Component/EditTask';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {TaskContextProvider} from'./context/TaskContext'
+import './Css/app.css'
 function App() {
   return (
    
-    <div>
-      <BrowserRouter>
-              <Navbar />
-              <div className="container">
-                <div className="row">  
-                    <div className="col-10 mx-auto">
-                      <Switch>
-                          <TaskContextProvider>
-                               <Route exact path="/" component={TaskList} />
-                              <Route exact path="/add-task" component={AddTask} />
-                          </TaskContextProvider>
-                      </Switch>
-                    </div>
-                </div> 
-              </div>
-      </BrowserRouter>
+      <div className="app">
+        <div className="overlay"></div>
+        <BrowserRouter>
+                  <Navbar />
+                  <div className="container">
+                    <div className="row">  
+                        <div className="col-10 mx-auto cont-app my-3">
+                          <Switch>
+                              <TaskContextProvider>
+                                  <Route exact path="/" component={TaskList} />
+                                  <Route exact path="/add-task" component={AddTask} />
+                              </TaskContextProvider>
+                          </Switch>
+                        </div>
+                    </div> 
+                  </div>
+        </BrowserRouter>
+        
     </div>
+
   );
 }
 

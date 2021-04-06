5b7126796f1c90ee56ebbc2315082b1660222fa4
Mina Nagy <minanagy1993@gmail.com>
2021-04-06 15:16:29 +0200

    Make Task Component

--- src/Component/TaskList.js
+++ src/Component/TaskList.js
@@ -1,6 +1,6 @@
 import React ,{useContext}from 'react'
 import { TaskContext} from '../context/TaskContext'
-
+import Task from './Task'
 export default function TaskList() {
     const{tasks}= useContext(TaskContext);
     console.log(tasks);
@@ -8,35 +8,40 @@ export default function TaskList() {
         <>
         <h1 className="text-center display-4 ">All Tasks</h1>
         <ul className="list-unstyled">
-           {tasks.map((item,index)=>{
-                return(<li className="border p-3 my-3" key={index}>
-                    {item.title}{/*Name of Task */}
-                    <button className="btn btn-danger float-right mr-2">Delete</button>
-                    <button className="btn btn-info float-right mr-2">Edit</button>
-                </li>)
-           })}
-           {/*
-               <li className="border p-3 my-3">
-                   Name of Task 
-                   <button className="btn btn-danger float-right mr-2">Delete</button>
-                   <button className="btn btn-info float-right mr-2">Edit</button>
-               </li>
-               
-               
-               <li className="border p-3 my-3">
-                   Name of Task 
-                   <button className="btn btn-danger float-right mr-2">Delete</button>
-                   <button className="btn btn-info float-right mr-2">Edit</button>
-               </li>
-   
-               <li className="border p-3 my-3">
-                   Name of Task 
-                   <button className="btn btn-danger float-right mr-2">Delete</button>
-                   <button className="btn btn-info float-right mr-2">Edit</button>
-               </li>
+                {
+                        tasks.map((item,index)=>{
+                            return(
+                                 <Task item={item} key={index}/>
+                                /*   <li className="border p-3 my-3" key={index}>
+                                    {item.title}
+                                    <button className="btn btn-danger float-right mr-2">Delete</button>
+                                    <button className="btn btn-info float-right mr-2">Edit</button>
+                                </li>*/
+                            )
+                         })
+                }
+                {/*
+                    <li className="border p-3 my-3">
+                        Name of Task 
+                        <button className="btn btn-danger float-right mr-2">Delete</button>
+                        <button className="btn btn-info float-right mr-2">Edit</button>
+                    </li>
+                    
+                    
+                    <li className="border p-3 my-3">
+                        Name of Task 
+                        <button className="btn btn-danger float-right mr-2">Delete</button>
+                        <button className="btn btn-info float-right mr-2">Edit</button>
+                    </li>
 
-           */} 
+                    <li className="border p-3 my-3">
+                        Name of Task 
+                        <button className="btn btn-danger float-right mr-2">Delete</button>
+                        <button className="btn btn-info float-right mr-2">Edit</button>
+                    </li>
+
+                */} 
         </ul>
         </>
-        )
+    )
  }  
